7c89dcbaafd9d7adcbabd8ffdce5a0b4d82a531c
EvanR <evanrinehart@gmail.com>
2015-03-11 11:35:16 -0500

    update the interface

--- Control/Broccoli.hs
+++ Control/Broccoli.hs
@@ -5,21 +5,20 @@
 module Control.Broccoli (
   X,
   E,
-  Output(..),
-  runProgram,
-  edge,
-  accumulate,
+  never,
   snapshot,
   snapshot_,
-  out,
+  accumulate,
+  edge,
   filterE,
   justE,
   maybeE,
-  never,
-  debugX,
-  debugE,
+  Setup,
+  runProgram,
   newX,
-  newE
+  newE,
+  input,
+  output,
 ) where
 
 import Control.Applicative
@@ -38,7 +37,7 @@ data X a where
   PureX :: a -> X a
   FmapX :: forall a b . (b -> a) -> X b -> X a
   ApplX :: forall a b . X (b -> a) -> X b -> X a
-  PortX :: TVar a -> X a
+  PortX :: MVar [ThreadId] -> TVar a -> X a
 
 -- | An event that carries values of type a when it occurs.
 data E a where
@@ -48,10 +47,7 @@ data E a where
   ProductE  :: (b -> c -> a) -> E b -> E c -> E a
   SnapshotE :: E b -> X a -> E a
   JustE     :: E (Maybe a) -> E a
-  PortE     :: TChan a -> E a
-
--- | A special IO action which will react to events. See 'out'.
-newtype Output = Output (IO ())
+  PortE     :: MVar [ThreadId] -> TChan a -> E a
 
 instance Functor X where
   fmap f x = FmapX f x
@@ -67,6 +63,47 @@ instance Monoid (E a) where
   mempty = NeverE
   mappend e1 e2 = MappendE e1 e2
 
+-- | A monad for hooking up inputs and outputs to a program.
+data Setup a = Setup (MVar [ThreadId] -> IO a)
+
+instance Monad Setup where
+  return x = Setup (\_ -> return x)
+  (Setup r) >>= f = Setup r' where
+    r' mv = do
+      x <- r mv
+      let Setup r'' = f x
+      r'' mv
+
+instance Applicative Setup where
+  pure = return
+  (<*>) = ap
+
+instance Functor Setup where
+  fmap f (Setup io) = Setup (\mv -> f <$> io mv)
+
+setupIO :: IO a -> Setup a
+setupIO io = Setup (\_ -> io)
+
+getThreads :: Setup (MVar [ThreadId])
+getThreads = Setup (\mv -> return mv)
+
+getThreadsE :: E a -> Maybe (MVar [ThreadId])
+getThreadsE e = case e of
+  NeverE -> Nothing
+  FmapE _ e' -> getThreadsE e'
+  MappendE e1 e2 -> getFirst $ First (getThreadsE e1) <> First (getThreadsE e2)
+  ProductE _ e1 e2 -> getFirst $ First (getThreadsE e1) <> First (getThreadsE e2)
+  SnapshotE e' x -> getFirst $ First (getThreadsE e') <> First (getThreadsX x)
+  JustE e' -> getThreadsE e'
+  PortE mv _ -> Just mv
+
+getThreadsX :: X a -> Maybe (MVar [ThreadId])
+getThreadsX x = case x of
+  PureX _ -> Nothing
+  FmapX _ x' -> getThreadsX x'
+  ApplX x1 x2 -> getFirst $ First (getThreadsX x1) <> First (getThreadsX x2)
+  PortX mv _ -> Just mv
+
 dupE :: E a -> IO (E a)
 dupE e = case e of
   NeverE -> return NeverE
@@ -77,9 +114,6 @@ dupE e = case e of
     e1' <- dupE e1
     e2' <- dupE e2
     return (MappendE e1' e2')
-  PortE ch -> do
-    ch' <- atomically (dupTChan ch)
-    return (PortE ch')
   ProductE f e1 e2 -> do
     e1' <- dupE e1
     e2' <- dupE e2
@@ -90,11 +124,13 @@ dupE e = case e of
   JustE e' -> do
     e'' <- dupE e'
     return (JustE e'')
+  PortE mv ch -> do
+    ch' <- atomically (dupTChan ch)
+    return (PortE mv ch')
 
 readE :: E a -> IO a
 readE e = case e of
   NeverE -> hang
-  PortE ch -> atomically (readTChan ch)
   MappendE e1 e2 -> race (readE e1) (readE e2)
   FmapE f e' -> f <$> readE e'
   ProductE f e1 e2 -> do
@@ -109,6 +145,7 @@ readE e = case e of
     case m of
       Nothing -> loop
       Just x  -> return x
+  PortE _ ch -> atomically (readTChan ch)
 
 readX :: X a -> STM a
 readX x = case x of
@@ -118,7 +155,7 @@ readX x = case x of
     f <- readX ff
     x <- readX xx
     return (f x)
-  PortX tv -> readTVar tv
+  PortX _ tv -> readTVar tv
 
 hang :: IO a
 hang = do
@@ -130,30 +167,7 @@ waitE e0 = do
   e <- dupE e0
   readE e
 
-runEvent :: E a -> (a -> IO ()) -> IO ()
-runEvent e0 act = do
-  e <- dupE e0
-  forever (readE e >>= act)
-
 ---
-
--- | Create a new signal with an initial value. Get back an IO action to
--- change the value of the signal. This is the only way the signal will
--- change.
-newX :: a -> IO (a -> IO (), X a)
-newX v0 = do
-  tv <- newTVarIO v0
-  return
-    ( \x -> atomically (writeTVar tv x)
-    , PortX tv )
-
--- | Create a new event and the associated IO action to trigger the event.
-newE :: IO (a -> IO (), E a)
-newE = do
-  ch <- newBroadcastTChanIO
-  return
-    ( \x -> atomically (writeTChan ch x)
-    , PortE ch )
 
 -- | An event which gets the value of a signal when another event occurs.
 snapshot :: E a -> X b -> E (a,b)
@@ -179,70 +193,94 @@ filterE p e = maybeE (\x -> if p x then Just x else Nothing) e
 never :: E a
 never = mempty
 
--- | Events will occur when an "edge" is detected in a signal. The detection
--- algorithm checks the two values before and after a discrete change in
--- the signal.
-edge :: X a -> (a -> a -> Maybe b) -> E b
-edge x diff = PortE ch where
-  ch = unsafePerformIO $ do
-    out <- newBroadcastTChanIO
-    forkIO $ do
-      v0 <- atomically (readX x)
-      ref <- newIORef v0
-      forever $ do
-        v <- readIORef ref
-        (d, v') <- atomically $ do
-          v' <- readX x
-          case diff v v' of
-            Just d  -> return (d, v')
-            Nothing -> retry
-        writeIORef ref v'
-        atomically (writeTChan out d)
-    return out
-
--- | Create a signal out of an input event and a state machine.
+-- | Sum over events using an initial state and a state transition function.
 accumulate :: E a -> s -> (a -> s -> s) -> X s
-accumulate e0 s0 trans = PortX tv where
-  tv = unsafePerformIO $ do
-    state <- newTVarIO s0
-    forkIO $ do
-      e <- dupE e0
-      forever $ do
-        x <- readE e
-        atomically $ do
-          s <- readTVar state
-          let s' = trans x s
-          writeTVar state s'
-    return state
+accumulate e0 s0 trans = case getThreadsE e0 of
+  Nothing -> pure s0
+  Just mv -> PortX mv tv where
+    tv = unsafePerformIO $ do
+      state <- newTVarIO s0
+      threadId <- forkIO $ do
+        putStrLn "accum forked"
+        e <- dupE e0
+        forever $ do
+          x <- readE e
+          atomically $ do
+            s <- readTVar state
+            let s' = trans x s
+            writeTVar state s'
+      modifyMVar_ mv (return . (threadId:))
+      return state
 
--- | A handler for events.
-out :: E a -> (a -> IO ()) -> Output
-out e0 act = Output (runEvent e0 act)
+-- | An event that occurs when an edge is detected in a signal. The edge test
+-- is applied to values before and after a discrete transition in the signal.
+-- The test should return Nothing when the two values are the same.
+edge :: X a -> (a -> a -> Maybe b) -> E b
+edge x diff = case getThreadsX x of
+  Nothing -> never
+  Just mv -> PortE mv ch where
+    ch = unsafePerformIO $ do
+      out <- newBroadcastTChanIO
+      threadId <- forkIO $ do
+        putStrLn "edge forked"
+        v0 <- atomically (readX x)
+        ref <- newIORef v0
+        forever $ do
+          v <- readIORef ref
+          (d, v') <- atomically $ do
+            v' <- readX x
+            case diff v v' of
+              Just d  -> return (d, v')
+              Nothing -> retry
+          writeIORef ref v'
+          atomically (writeTChan out d)
+      modifyMVar_ mv (return . (threadId:))
+      return out
 
--- | Prints the values of a signal as they change.
-debugX :: (Eq a, Show a) => X a -> Output
-debugX x = Output $ do
-  v0 <- atomically (readX x)
-  print v0
-  let diff a b = if a == b then Nothing else Just b
-  runEvent (edge x diff) print
 
--- | Prints the values of events as they occur.
-debugE :: (Show a) => E a -> Output
-debugE e = out e print
+-- | Creates a new event and an IO action to trigger it.
+newE :: Setup (E a, a -> IO ())
+newE = do
+  mv <- getThreads
+  bch <- setupIO newBroadcastTChanIO
+  return (PortE mv bch, atomically . writeTChan bch)
 
--- | Run a set of Outputs. This spawns several threads then waits for an
--- event. The output threads will then be killed to stop further processing.
--- However other threads which no longer have any effect will probably remain,
--- taking up resources. After threads have been spawned but before waiting,
--- the given "boot" action will be executed.
-runProgram :: IO () -- ^ action to execute after initialization
-           -> E ()  -- ^ event that will shutdown the system
-           -> [Output] -- ^ set of output event handlers to run
-           -> IO ()
-runProgram notifyBoot stop outs = do
-  tids <- forM outs (\(Output io) -> forkIO io)
+-- | Creates a new signal and an IO action to update it.
+newX :: a -> Setup (X a, a -> IO ())
+newX v = do
+  mv <- getThreads
+  tv <- setupIO (newTVarIO v)
+  return (PortX mv tv, atomically . writeTVar tv)
+
+-- | Spawn a thread to execute an action for each event occurrence.
+output :: E a -> (a -> IO ()) -> Setup ()
+output e0 act = do
+  mv <- getThreads
+  setupIO $ do
+    e <- dupE e0
+    tid <- (forkIO . forever) (readE e >>= act)
+    modifyMVar_ mv (return . (tid:))
+    return ()
+
+-- | Spawn an input thread to generate source signals and events.
+input :: IO () -> Setup ()
+input handler = do
+  mv <- getThreads
+  setupIO $ do
+    tid <- forkIO handler
+    modifyMVar_ mv (return . (tid:))
+    return ()
+
+-- | Run the setup action to create input and output threads. The returned IO
+-- action will be executed when setup is complete. runProgram blocks until
+-- the returned event occurs, at which time it kills all the threads and
+-- returns.
+runProgram :: Setup (IO (), E ()) -> IO ()
+runProgram (Setup setup) = do
+  mv <- newMVar []
+  (boot, exit) <- setup mv
   threadDelay 5000
-  notifyBoot
-  waitE stop
-  forM_ tids killThread
+  boot
+  waitE exit
+  withMVar mv (mapM killThread)
+  return ()
